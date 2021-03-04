cac3b360dd0fe51617b9e1dbbdda6b3c43f6f7f0
Anshil Saxena <Anshil.Saxena@zimozi.co>
2021-03-04 12:53:16 +0530

    my second reactapp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StarMatch from './StarMatch';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <StarMatch />
+  </>,
   document.getElementById('root')
 );
 
